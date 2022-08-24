23458db72e1f4679b70d9885f9650ec95744e3ef
TypeErrorDev <matthew.pantel86@gmail.com>
2022-08-24 23:53:29 +0300

    ..

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require("express");
-// const routes = require("./routes");
-const { projects } = require("./data/projects.json");
 const app = express();
 
 // Allows exress to use the Public folder as a Static folder
@@ -9,31 +7,10 @@ app.use("./static", express.static("./public"));
 // Allows express to use pug templates
 app.set("view engine", "pug");
 
-// GET REQUESTS
+// Allows express to use the routes folder
+const routes = require("./routes");
 
-// Setting up the root Route
-// TODO - Move to routes.js
-app.get("/", (req, res, next) => {
-  res.render("index", { projects });
-  next();
-});
-
-// Routes to the About Page
-app.get("/about", (req, res, next) => {
-  res.render("about");
-  next();
-});
-
-// Routes to the Projects Page
-app.get("/project/:id", (req, res, next) => {
-  if (projects[req.params.id]) {
-    res.render("project", {
-      project: projects[req.params.id],
-    });
-  } else {
-    next();
-  }
-});
+app.use(routes);
 
 // Turn on Express server
 app.listen(3000, () => {
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const { projects } = require("./data/projects.json");
+
+// Setting up the root Route
+// TODO - Move to routes.js
+router.get("/", (req, res, next) => {
+  res.render("index", { projects });
+  next();
+});
+
+// Routes to the About Page
+router.get("/about", (req, res, next) => {
+  res.render("about");
+  next();
+});
+
+// Routes to the Projects Page
+router.get("/project/:id", (req, res, next) => {
+  if (projects[req.params.id]) {
+    res.render("project", {
+      project: projects[req.params.id],
+    });
+  } else {
+    next();
+  }
+});
+
+module.exports = router;
