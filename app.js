6cc5f25491766287bceb133ce3d74ac422b1a2f9
TypeErrorDev <matthew.pantel86@gmail.com>
2022-08-25 00:02:06 +0300

    RESTORE POINT: root file works

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
-=======
-// const routes = require("./routes");
-const { projects } = require("./data/data.json");
->>>>>>> parent of 70d19a0 (FIX: project to projects)
 const app = express();
 
 // Allows exress to use the Public folder as a Static folder
@@ -13,28 +8,9 @@ app.use("./static", express.static("./public"));
 app.set("view engine", "pug");
 
 // Allows express to use the routes folder
-const routes = require("./routes");
+const routes = require(".");
 
-<<<<<<< HEAD
 app.use(routes);
-=======
-// Routes to the About Page
-app.get("/about", (req, res) => {
-  res.render("about");
-  next();
-});
-
-// Routes to the Projects Page
-app.get("/project:id", (req, res) => {
-  if (projects[req.params.id]) {
-    res.render("project", {
-      project: projects[req.params.id],
-    });
-  } else {
-    next();
-  }
-});
->>>>>>> parent of 70d19a0 (FIX: project to projects)
 
 // Turn on Express server
 app.listen(3000, () => {
