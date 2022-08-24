75f56ead25872ce11f72c91b5b77cc6aafd672b8
TypeErrorDev <matthew.pantel86@gmail.com>
2022-08-24 22:44:38 +0300

    .

--- app.js
+++ app.js
@@ -4,29 +4,31 @@ const { projects } = require("./data/projects.json");
 const app = express();
 
 // Allows exress to use the Public folder as a Static folder
-app.set("./static", express.static("./public"));
+app.use("./static", express.static("./public"));
 
 // Allows express to use pug templates
 app.set("view engine", "pug");
 
+// GET REQUESTS
+
 // Setting up the root Route
 // TODO - Move to routes.js
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", (req, res, next) => {
+  res.render("index", { projects });
   next();
 });
 
 // Routes to the About Page
-app.get("/about", (req, res) => {
+app.get("/about", (req, res, next) => {
   res.render("about");
   next();
 });
 
 // Routes to the Projects Page
-app.get("/project:id", (req, res) => {
+app.get("/project/:id", (req, res, next) => {
   if (projects[req.params.id]) {
-    res.render("projects", {
-      projects: projects[req.params.id],
+    res.render("project", {
+      project: projects[req.params.id],
     });
   } else {
     next();
