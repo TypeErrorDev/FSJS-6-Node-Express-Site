d06ad169cd170dab23595e4abbb33e6cb0bfd8f6
TypeErrorDev <matthew.pantel86@gmail.com>
2022-08-27 23:11:21 +0300

    I believe I'm complete with the Error handling, but not sure

--- index.js
+++ index.js
@@ -19,13 +19,33 @@ router.get("/about", (req, res, next) => {
 router.get("/projects/:id", (req, res, next) => {
   const projectId = req.params.id;
   const project = projects.find(({ id }) => id === +projectId);
-  if (project) {
-    res.render("projects", { project });
+  if (!project) {
+    res.status(404).render("page-not-found");
+    next(err);
   } else {
-    const err = new Error("not found");
-    err.status = 404;
-    err.message = `Looks like the quote you requested doesn't exist`;
-    res.render("error", { err });
+    res.render("projects", { project });
+  }
+});
+
+// Undefined Error Handler
+router.use((req, res, next) => {
+  res.status(404).render("page-not-found");
+  next();
+});
+
+// Server Error Handler
+router.use((err, req, res, next) => {
+  if (err) {
+    console.log("Generic Error Handler: ", err);
+  }
+  if (err.status === 404) {
+    res.status(404);
+    res.render("error", { error: err });
+  } else {
+    console.log(`Server Error Occured`);
+    err.message = err.message || `Catastrophic Server Error Occured`;
+    res.status(err.status || 500);
+    res.render("error", { error: err });
   }
 });
 
