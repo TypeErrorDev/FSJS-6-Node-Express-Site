ab104057716e50c7e9ead3523fc92d2176e7f4b7
TypeErrorDev <matthew.pantel86@gmail.com>
2022-08-31 22:23:43 +0200

    COMPLETED PROJECT

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 const { projects } = require("./data/projects.json");
@@ -6,16 +7,15 @@ const { projects } = require("./data/projects.json");
 // TODO - Move to routes.js
 router.get("/", (req, res, next) => {
   console.log("DEBUG: home page");
-  res.render("index", { projects });
-  next();
+  return res.render("index", { projects });
 });
 
 // Routes to the About Page
 router.get("/about", (req, res, next) => {
   console.log("DEBUG: about page");
-  res.status(500);
-  res.render("about");
-  next();
+  //  res.status(500); // For reviewer to test 500 - Internal Server Error
+  return res.render("about");
+  // next(err); // pass the error to the next error handler
 });
 
 // Routes to the Projects Page
@@ -24,7 +24,7 @@ router.get("/projects/:id", (req, res, next) => {
   const project = projects.find(({ id }) => id === +projectId);
   if (project) {
     console.log("DEBUG: project page");
-    res.render("projects", { project });
+    return res.render("projects", { project });
   } else if (!project) {
     next();
   }
@@ -32,25 +32,17 @@ router.get("/projects/:id", (req, res, next) => {
 
 // Global error handler
 router.use((err, req, res, next) => {
-  if (res.status === 500) {
+  if (err) {
     console.log("DEBUG: Server error handler");
     err.message = "Generic Error: " + err.message;
-    res.status(500);
-    res.render("error500", { error: err });
-  }
-  if (res.status === 404) {
-    console.log("DEBUG: global 404 error handler");
-    const err = new Error("Generic Error: Page not found");
-    res.status(404);
-    res.render("error404", { error: err });
+    return res.render("error500", { error: err });
   }
 });
-// router.use((req, res, next) => {
-//   console.log("DEBUG: global 404 error handler");
-//   const err = new Error("Generic Error: Page not found");
-//   res.status(404);
-//   res.render("error404", { error: err });
-// });
+router.use((req, res, next) => {
+  console.log("DEBUG: global 404 error handler");
+  const err = new Error("Generic Error: Page not found");
+  return res.status(404).render("error404", { error: err });
+});
 
 // Export the router
 module.exports = router;
