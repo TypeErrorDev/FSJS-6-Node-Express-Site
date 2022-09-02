0e2a281d46dc2f1d70de5744025c63dbb992a54e
TypeErrorDev <matthew.pantel86@gmail.com>
2022-09-02 02:14:21 +0200

    FIX: pug template iterating over technologies

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ router.get("/about", (req, res, next) => {
 router.get("/projects/:id", (req, res, next) => {
   const projectId = req.params.id;
   const project = projects[projectId];
-
   if (project) {
     console.log(`DEBUG: You are at the project/${projectId} page`);
     return res.render("projects", { project });
@@ -30,25 +29,25 @@ router.get("/projects/:id", (req, res, next) => {
   }
 });
 
-// router.use((req, res, next) => {
-//   console.log("DEBUG: You've hit the error catch ");
-//   const err = new Error("Generic Error: Page not found");
-//   next(err);
-// });
+router.use((req, res, next) => {
+  console.log("DEBUG: You've hit the error catch ");
+  const err = new Error("Generic Error: Page not found");
+  next(err);
+});
 
-// // Global error handler
-// router.use((err, req, res, next) => {
-//   console.log("DEBUG: You've hit the error handler middleware");
-//   if (err.status == 404) {
-//     console.log("DEBUG: You've hit the 404 error if statement");
-//     return res.render("error404", { error: err });
-//   } else {
-//     console.log("DEBUG: You've hit the 500 error else statement");
-//     const err = new Error("Generic Error: This Page is not found");
-//     err.status = 500;
-//     return res.render("error500", { error: err });
-//   }
-// });
+// Global error handler
+router.use((err, req, res, next) => {
+  console.log("DEBUG: You've hit the error handler middleware");
+  if (err.status == 404) {
+    console.log("DEBUG: You've hit the 404 error if statement");
+    return res.render("error404", { error: err });
+  } else {
+    console.log("DEBUG: You've hit the 500 error else statement");
+    const err = new Error("Generic Error: This Page is not found");
+    err.status = 500;
+    return res.render("error500", { error: err });
+  }
+});
 
 // Export the router
 module.exports = router;
